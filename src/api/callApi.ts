d7a1a4e4f6ed3ca417f59a000449650f90c36a78
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-09 16:44:57 +0900

    getAllPostのAPIを作成

--- src/api/callApi.ts
+++ src/api/callApi.ts
@@ -1,4 +1,10 @@
-import { CreatePostRequest, CreatePostResponse, isCreatePostResponse } from './types';
+import {
+  CreatePostRequest,
+  CreatePostResponse,
+  GetAllPostResponse,
+  isCreatePostResponse,
+  isGetAllPostResponse,
+} from './types';
 import { API } from 'aws-amplify';
 
 export const createPost = async (request: CreatePostRequest, authToken: string): Promise<CreatePostResponse | null> => {
@@ -10,6 +16,19 @@ export const createPost = async (request: CreatePostRequest, authToken: string):
   };
   const response = (await API.post('api', '/posts', payload)) as unknown;
   if (isCreatePostResponse(response)) {
+    return response;
+  }
+  return null;
+};
+
+export const getAllPost = async (authToken: string): Promise<GetAllPostResponse | null> => {
+  const payload = {
+    headers: {
+      Authorization: authToken,
+    },
+  };
+  const response = (await API.get('api', '/posts', payload)) as unknown;
+  if (isGetAllPostResponse(response)) {
     return response;
   }
   return null;
