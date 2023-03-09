44ad6e7ac18e7e5a4b5d739dc0de17e0c503669f
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-09 16:35:13 +0900

    TimeLineコンポーネントをざっくり作成

---
+++ src/components/Posts/TimeLine.tsx
@@ -0,0 +1,21 @@
+import { List, ListItem, ListItemText } from '@mui/material';
+import { Post } from '../../api/types';
+
+type Props = {
+  posts: Post[];
+};
+
+const viewPost = (post: Post) => {
+  return (
+    <ListItem key={post.id}>
+      <ListItemText primary={post.content.comment} secondary={post.userId} />
+    </ListItem>
+  );
+};
+
+const TimeLine = (props: Props) => {
+  const { posts } = props;
+  return <List>{posts.map((post) => viewPost(post))}</List>;
+};
+
+export default TimeLine;
