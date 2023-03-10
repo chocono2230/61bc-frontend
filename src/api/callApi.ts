565d97c9b208fce778ae7621551b171eb603aab1
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-10 16:41:20 +0900

    User作成APIの実装

--- src/api/callApi.ts
+++ src/api/callApi.ts
@@ -6,6 +6,7 @@ import {
   isGetAllPostResponse,
 } from './types/post';
 import { API } from 'aws-amplify';
+import { CreateUserRequest, CreateUserResponse, isCreateUserResponse } from './types/user';
 
 export const createPost = async (request: CreatePostRequest, authToken: string): Promise<CreatePostResponse | null> => {
   const payload = {
@@ -30,6 +31,20 @@ export const getAllPost = async (authToken: string, userId?: string): Promise<Ge
   const path = userId ? `/posts/${userId}` : '/posts';
   const response = (await API.get('api', path, payload)) as unknown;
   if (isGetAllPostResponse(response)) {
+    return response;
+  }
+  return null;
+};
+
+export const createUser = async (request: CreateUserRequest, authToken: string): Promise<CreateUserResponse | null> => {
+  const payload = {
+    headers: {
+      Authorization: authToken,
+    },
+    body: request,
+  };
+  const response = (await API.post('api', '/users', payload)) as unknown;
+  if (isCreateUserResponse(response)) {
     return response;
   }
   return null;
