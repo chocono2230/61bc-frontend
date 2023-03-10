2012d6f50030f37cbde4e73a44fd08526ff2d698
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-10 17:15:48 +0900

    Appの内容をTopへ移動して修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { Container } from '@mui/material';
 
 import awsExports from './aws-exports';
 import awsmobilemanual from './aws-exports-manual';
+import Top from './Top';
+
 Amplify.configure(awsExports);
 Amplify.configure(awsmobilemanual);
-
-import Test from './components/Test';
-import Home from './pages/Home';
 
 const App = () => {
   return (
     <Authenticator loginMechanisms={['username']} hideSignUp={true}>
-      <Container maxWidth='sm'>
-        <Test />
-        <Home />
-      </Container>
+      <Top />
     </Authenticator>
   );
 };
