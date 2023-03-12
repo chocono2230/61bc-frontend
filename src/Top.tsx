58c7f1d0990dfe33f731c74514117f028147455c
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-12 14:04:08 +0900

    ログイン時にUsers取得

--- src/Top.tsx
+++ src/Top.tsx
@@ -2,17 +2,20 @@ import { useState, useEffect, createContext } from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { Container } from '@mui/material';
 
-import { CreateUserRequest, User } from './api/types/user';
-import { createUser } from './api/callApi';
+import { CreateUserRequest, PublicUser, User } from './api/types/user';
+import { createUser, getAllPublicUser } from './api/callApi';
 import Test from './components/Test';
 import Home from './pages/Home';
 
 export const UserContext = createContext<User | null>(null);
+export const UsersMapContext = createContext<Map<string, PublicUser> | null>(null);
 
 const Top = () => {
   const [iuser, setIuser] = useState<User | null>(null);
+  const [usersMap, setUsersMap] = useState<Map<string, PublicUser> | null>(null);
   const { user } = useAuthenticator((context) => [context.user]);
   const token = user.getSignInUserSession()?.getIdToken().getJwtToken();
+
   useEffect(() => {
     void (async () => {
       const request: CreateUserRequest = {
@@ -31,12 +34,32 @@ const Top = () => {
     })();
   }, [token, user]);
 
+  useEffect(() => {
+    void (async () => {
+      if (!token) return;
+      try {
+        const res = await getAllPublicUser(token);
+        if (res) {
+          const map = new Map<string, PublicUser>();
+          res.users.forEach((u) => {
+            map.set(u.id, u);
+          });
+          setUsersMap(map);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [token]);
+
   if (!user) return <></>;
   return (
     <Container maxWidth='sm'>
       <UserContext.Provider value={iuser}>
-        <Test />
-        <Home />
+        <UsersMapContext.Provider value={usersMap}>
+          <Test />
+          <Home />
+        </UsersMapContext.Provider>
       </UserContext.Provider>
     </Container>
   );
