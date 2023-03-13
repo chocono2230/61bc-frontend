80f52636baf112684867a56d53a619b35c94f041
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-13 15:58:21 +0900

    ユーザページの完成

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,8 +1,56 @@
+import { useState, useEffect, useContext } from 'react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { useParams } from 'react-router-dom';
 
+import { UsersMapContext } from '../../Top';
+import { getAllPost } from '../../api/callApi';
+import { Post } from '../../api/types/post';
+import TimeLine from '../../components/Posts/TimeLine';
+import CustomizedSnackbar from '../../components/CustomizedSnackbar';
+
 const User = () => {
+  const token = useAuthenticator((context) => [context.user])
+    .user.getSignInUserSession()
+    ?.getIdToken()
+    .getJwtToken();
+  const usersMap = useContext(UsersMapContext);
   const { id } = useParams();
-  return <>ユーザページ{id}</>;
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [unknownUser, setUnknownUser] = useState<boolean>(false);
+  const [faildLoading, setFaildLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    void (async () => {
+      if (!token) return;
+      try {
+        const res = await getAllPost(token, id);
+        if (res) {
+          setPosts(res.posts);
+        }
+      } catch (err) {
+        console.error(err);
+        setFaildLoading(true);
+      }
+    })();
+  }, [id, token]);
+
+  return (
+    <>
+      <TimeLine posts={posts} users={usersMap} setUnknownUser={setUnknownUser} />
+      <CustomizedSnackbar
+        msg={'リロードしてユーザ情報を更新してください'}
+        serverity={'warning'}
+        open={unknownUser}
+        setOpen={setUnknownUser}
+      />
+      <CustomizedSnackbar
+        msg={'投稿の取得に失敗しました'}
+        serverity={'error'}
+        open={faildLoading}
+        setOpen={setFaildLoading}
+      />
+    </>
+  );
 };
 
 export default User;
