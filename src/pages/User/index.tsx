2cf86a171b28f726ac5882eb8ab661d1535d2ef7
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-14 18:46:08 +0900

    ログインしているユーザのページなら削除可能にする #37 (#38)

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { useParams } from 'react-router-dom';
 
+import { UserContext } from '../../Top';
 import { UsersMapContext } from '../../Top';
 import { getAllPost } from '../../api/callApi';
 import { Post } from '../../api/types/post';
@@ -14,10 +15,13 @@ const User = () => {
     ?.getIdToken()
     .getJwtToken();
   const usersMapContext = useContext(UsersMapContext);
+  const userContext = useContext(UserContext);
   const { id } = useParams();
   const [posts, setPosts] = useState<Post[]>([]);
   const [unknownUser, setUnknownUser] = useState<boolean>(false);
   const [faildLoading, setFaildLoading] = useState<boolean>(false);
+  const [deletePost, setDeletePost] = useState<boolean>(false);
+  const [deletePostId, setDeletePostId] = useState<string>('');
 
   useEffect(() => {
     void (async () => {
@@ -34,14 +38,27 @@ const User = () => {
     })();
   }, [id, token]);
 
+  useEffect(() => {
+    if (deletePostId === '') return;
+    setPosts((prev) => prev.filter((p) => p.id !== deletePostId));
+    setDeletePost(true);
+  }, [deletePostId]);
+
   return (
     <>
-      <TimeLine posts={posts} users={usersMapContext?.usersMap ?? null} setUnknownUser={setUnknownUser} />
+      <TimeLine posts={posts} users={usersMapContext?.usersMap ?? null} setUnknownUser={setUnknownUser} userId={userContext?.user?.id} authToken={token} identity={userContext?.user?.identity} setDeletePostId={setDeletePostId} />
       <CustomizedSnackbar
         msg={'リロードしてユーザ情報を更新してください'}
         severity={'warning'}
         open={unknownUser}
         setOpen={setUnknownUser}
+      />
+      <CustomizedSnackbar
+        msg={'投稿を削除しました'}
+        severity={'success'}
+        open={deletePost}
+        setOpen={setDeletePost}
+        time={2000}
       />
       <CustomizedSnackbar
         msg={'投稿の取得に失敗しました'}
